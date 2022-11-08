32684b79f932adcaaee83c0af7b108b6d16c227f
Nazmush Shakib <shakibgithub944>
2022-11-08 13:06:04 +0600

    created api for all services

--- index.js
+++ index.js
@@ -13,22 +13,32 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-function run() {
+ async function run() {
     try {
         const serviceCollection = client.db("pose-n-click").collection("services");
-       
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services)
+
+        })
+        app.get('/all-services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+
+        })
 
     }
     finally {
 
     }
 }
-run().catch(err => console.log(err));
 
-app.get('/', (req, res) => {
-    res.send('Pose N Click server is running')
-})
-
+run().catch(err => console.error(err))
 
 
 
