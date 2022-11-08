05c38deb87f3f58d1abe192339e767c63f1e78b0
Nazmush Shakib <shakibgithub944>
2022-11-08 12:15:45 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
-
+require('dotenv').config()
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ofvswtt.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function run() {
+    try {
+        const serviceCollection = client.db("pose-n-click").collection("services");
+       
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.log(err));
 
 app.get('/', (req, res) => {
     res.send('Pose N Click server is running')
