56b47861041d2ad7bc52c79510d31e9d3e7b0830
Nazmush Shakib <shakibgithub944>
2022-11-08 13:26:49 +0600

    created api for specific services

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 const app = express();
@@ -13,7 +13,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
- async function run() {
+async function run() {
     try {
         const serviceCollection = client.db("pose-n-click").collection("services");
 
@@ -31,6 +31,16 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
             res.send(services)
 
         })
+        app.get('/all-services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result =await serviceCollection.findOne(query);
+            // const services = await cursor.toArray();
+            res.send(result)
+
+        })
+
+
 
     }
     finally {
